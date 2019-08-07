0c2ed739b6fc857f15b7b3b6e64296d6ebe9c6fb
Francela Perez <francelaperez24@gmail.com>
2019-08-06 21:01:57 -0600

    Agregando elementos
    Agregando el proyecto con algunas vistas integradas

---
+++ VotacionUCAWebApplication/Controllers/InicioController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VotacionUCAWebApplication.Controllers
+{
+    public class InicioController : Controller
+    {
+        public ActionResult Acceso()
+        {
+            return View();
+        }
+        public ActionResult Gestion()
+        {
+            return View();
+        }
+        public ActionResult Votaciones()
+        {
+            return View();
+        }
+    }
+}
