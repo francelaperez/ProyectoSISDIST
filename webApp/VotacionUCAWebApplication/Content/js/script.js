66b3b1b011776ee44fc1f0a43cf613063e12133d
JJ <jj.orozcolopez.13@gmail.com>
2019-09-06 21:10:58 -0600

    Actualización de prototipo final

--- webApp/VotacionUCAWebApplication/Content/js/script.js
+++ webApp/VotacionUCAWebApplication/Content/js/script.js
@@ -83,9 +83,9 @@ function ListarVotaciones() {
                 + '<td>' + this.CodGrupo + '</td>'
                 + '<td>' + estado + '</td>'
                 + '<td>'
-                + '<a href= "../Votaciones/Candidatos/?v=' + this.Id + '" class="btn-sm btn-warning"><i class="lni-eye" style="color:white"></i></a>'
-                + '<a href="../Votaciones/Editar/?v=' + this.Id + '" class="btn-sm btn-primary"><i class="lni-pencil"></i></a>'
-                + '<a href="#" onclick="Advertencia(' + this.Id + ')" class="btn-sm btn-danger"><i class="lni-trash"></i></a>'
+                + '<a href= "../Votaciones/Candidatos/?v=' + this.Id + '" class="btn-sm btn-warning"><i class="lni-eye" title= "Visualizar" style="color:white"></i></a>'
+                + '<a href="../Votaciones/Editar/?v=' + this.Id + '" class="btn-sm btn-primary"><i class="lni-pencil" title= "Editar"></i></a>'
+                + '<a href="#" onclick="Advertencia(' + this.Id + ')" class="btn-sm btn-danger"><i class="lni-trash" title= "Eliminar"></i></a>'
                 + '</td>'
                 + '<tr>'
                 
@@ -261,12 +261,14 @@ function BorrarVotacion(idVotacion) {
     var data = { IdVotacion: idVotacion };
     // POST usando AJAX y JQuery
     $.post('/ajax/borrarvotacion', data).done(function (resp) {
-        Swal.fire(
-            'Eliminado!',
-            'El registro se eliminó con éxito',
-            'success'
-        );
-        location.href = '/inicio/gestion';
+        Swal.fire({
+            type: 'success',
+            title: 'Eliminado!',
+            text: 'El registro se eliminó con éxito.'
+        }).then((resp) => {
+            location.href = '/inicio/gestion';
+        });
+
     });
 }
 
@@ -288,5 +290,65 @@ function CargarDatosVotacion() {
         $('#Nombre').val(resp.Descripcion);
         $('#Codigo').val(resp.CodGrupo);
         $('#Abierta').prop('checked', resp.Abierto);
+        
+        ListadoEstudiantesEdicion();
     });
+}
+
+
+function ListadoEstudiantesEdicion() {
+
+    // GET usando AJAX y JQuery
+    $.get('/Ajax/ListarEstudiantes').done(function (resp) {
+        var contador = 1;
+
+        $.each(resp, function () {
+            if (contador > 12) {
+                contador = 1;
+            }
+
+            $("#datosTablaE").append(
+                '<div class="col-lg-3 col-md-4 col-xs-13">'
+                + '<div class="category-icon-item lis-bg' + contador + '">'
+                + '<div class="icon-box">'
+                + '<i class="lni-user"></i>'
+                + '<h4>' + this.NombreCompleto + '</h4>'
+                + '<p class="categories-listing">Carnet:' + this.NumCarnet + '</p>'
+                + '<label class="btn btn-default">'
+                + '<input data-idE="' + this.Id + '" type="checkbox" autocomplete="off">'
+                + '</label>'
+                + '</div>'
+                + '</div>'
+                + '</div>'
+            );
+
+            contador = contador + 1;
+        });
+
+        CargarSeleccionCandidatos();
+
+    }).fail();
+}
+
+
+function CargarSeleccionCandidatos() {
+    var urlenlace = new URL(location.href);
+    var idVotacion = urlenlace.searchParams.get("v");
+    // GET usando AJAX y JQuery
+    $.get('/Ajax/ListarCandidatosVotacion/' + idVotacion).done(function (resp) {
+    
+    var inpts = $("#datosTablaE :input");
+        debugger;
+    resp.forEach(function (item) {
+        for (let elem of inpts) {
+            var id = $(elem).attr('data-idE');
+
+            if (id === item.IdEstudiante.toString()) {
+                $(elem).attr('checked', 'checked');
+            }
+        }
+
+        });
+
+    }).fail();
 }
